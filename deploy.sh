60e666594acb2a68d379ddb815e5902ce30a4c84
getcha22 <getcha576503369@gmail.com>
2018-08-09 08:26:33 +0800

    auto commit -----

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "-------------------------------------------------"
+echo "--------Start to deploy, running deploy.sh-------"
+echo "-------------------------------------------------"
+npm run build
+git add *
+read -t 30 -p "请输入commit信息：" commit
+git commit -m "auto commit ----- $commit"
+git push origin master
