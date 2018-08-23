26026cc88daf959366c886d6161d9318c65aebda
getcha22 <getcha576503369@gmail.com>
2018-08-23 13:03:39 +0800

    修改Banner部分的注意提示

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -6,7 +6,6 @@ export default function Banner ({children}) {
     const dataSource = [
         "1. 请使用最新版谷歌浏览器（Chrome Web Browser）或火狐浏览器（Firefox Web Browser）。",
         "2. 正常启动会提示 “初始化成功” 字样，若未出现提示，请检查麦克风是否正常，并使用推荐的浏览器。",
-        "3. 目前未设置端点检测，请在暂停使用时按下暂停键。"
     ];
     return (
         <Row className={styles.banner}>
