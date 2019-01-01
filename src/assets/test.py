c0dab1f0dbf509d1ef59eacf70708c21e58b9200
haoxiangsnr <haoxiangsnr@gmail.com>
2019-01-01 11:39:34 +0800

    initial demo

--- src/assets/test.py
+++
@@ -1,9 +0,0 @@
-import sys
-import requests
-multipart_form_data = {
-    'wave': ('wav.wav', open(sys.argv[1], 'rb')),
-}
-
-response = requests.post('http://202.207.12.156:8000/asr', {'fs': 48000, 'ch': 1}, files=multipart_form_data)
-
-print response.content
